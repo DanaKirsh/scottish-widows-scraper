028de9c2b9d5f1f5a8f1bd4592bf596f051c4c29
DanaKirsh <kirsh.dana@gmail.com>
2020-12-30 07:47:42 +0000

    Run first lint

--- index.js
+++ index.js
@@ -1,138 +1,136 @@
 const puppeteer = require('puppeteer');
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const {GoogleSpreadsheet} = require('google-spreadsheet');
 
 require('dotenv').config();
 
 (async () => {
+  // Navigate to the Scottish Widows website:
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(process.env.PENSION_URL);
 
-	// Navigate to the Scottish Widows website:
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto(process.env.PENSION_URL);
+  async function fillInputField(fieldName, input) {
+    await page.click(`input[name=${fieldName}]`);
+    await page.keyboard.type(input);
+  }
 
-	async function fillInputField(fieldName, input) {
-		await page.click(`input[name=${fieldName}]`);
-		await page.keyboard.type(input);
-	}
+  async function getText(dataSelector) {
+    const element = await page.$(`[data-selector=${dataSelector}]`);
+    return await element.evaluate((element) => element.innerText);
+  }
 
-	async function getElementText(dataSelector) {
-		const element = await page.$(`[data-selector=${dataSelector}]`);
-		return await element.evaluate(element => element.innerText);
-	}
+  if (!await page.$('h1[value*=\'SITE MAINTENANCE\']')) {
+    // Log into Scottish Widows:
+    await fillInputField('emailAddress', process.env.PENSION_EMAIL);
+    await fillInputField('password', process.env.PENSION_PASSWORD);
+    await Promise.all([
+      page.click('#button-login'),
+      page.waitForNavigation(),
+    ]);
 
-	if (!await page.$("h1[value*='SITE MAINTENANCE']")) {
+    // Scrape values:
+    await page.waitForSelector('[data-selector=manage-totalvalue]');
+    const dateText = await getText('manage-valuedate');
+    const newBalanceText = await getText('manage-totalvalue');
+    const premiumText = await getText('policy-card-last-premium-paid');
 
-		// Log into Scottish Widows:
-		await fillInputField("emailAddress", process.env.PENSION_EMAIL);
-		await fillInputField("password", process.env.PENSION_PASSWORD);
-		await Promise.all([
-			page.click("#button-login"),
-			page.waitForNavigation(),
-		]);
+    // Process values:
+    const date = getDateStr(dateText);
+    const premiumValue = parseFloat(premiumText.match(/\d+(\.\d+)?/)[0]);
+    const premiumDate = getDateStr(premiumText.match(/\d+ [A-Z][a-z]+ \d{4}/));
 
-		// Scrape values:
-		await page.waitForSelector('[data-selector=manage-totalvalue]');
-		const dateText = await getElementText("manage-valuedate");
-		const newBalanceText = await getElementText("manage-totalvalue");
-		const premiumPaidText = await getElementText("policy-card-last-premium-paid");
+    // Record data in Google Sheet:
+    addDataToSheet(date, newBalanceText, premiumValue, premiumDate);
+  }
 
-		// Process values:
-		const date = getFormattedDate(dateText);
-		const premiumValue = parseFloat(premiumPaidText.match(/\d+(\.\d+)?/)[0]);
-		const premiumDate = getFormattedDate(premiumPaidText.match(/\d+ [A-Z][a-z]+ \d\d\d\d/));
-
-		// Record data in Google Sheet:
-		addDataToSheet(date, newBalanceText, premiumValue, premiumDate);
-	}
-
-	await browser.close();
+  await browser.close();
 })();
 
 async function addDataToSheet(date, newBalance, premiumValue, premiumDate) {
+  // Load Google Sheet and authenticate:
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+  await doc.useServiceAccountAuth({
+    client_email: process.env.CLIENT_EMAIL,
+    private_key: process.env.PRIVATE_KEY,
+  });
+  await doc.loadInfo();
 
-	// Load Google Sheet and authenticate:
-	const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
-	await doc.useServiceAccountAuth({
-		client_email: process.env.CLIENT_EMAIL,
-		private_key: process.env.PRIVATE_KEY,
-	});
-	await doc.loadInfo();
+  // Find empty row to fill:
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows({limit: 15});
 
-	// Find empty row to fill:
-	const sheet = doc.sheetsByIndex[0];
-	const rows = await sheet.getRows({ limit: 15 });
-	const lastRecordIndex = rows.findIndex(row => row.date); // assuming not -1, based on my existing spreadsheet
-	var lastRow = rows[lastRecordIndex];
+  // assuming not -1, based on my existing spreadsheet
+  const lastRecordIndex = rows.findIndex((row) => row.date);
+  let lastRow = rows[lastRecordIndex];
 
-	if (rows.length > 0 && lastRow.date === date && lastRow.value === newBalance) {
-		console.log(`row ${date}: ${newBalance} already recorded. abort.`);
-	}
-	else {
-		var newRow = rows[lastRecordIndex - 1];
-		const time = new Date().toISOString().substr(11, 8);
-		const oldBalance = getDouble(lastRow.value);
-		var totalPaid = getDouble(lastRow["total payments"]);
-		newBalance = getDouble(newBalance);
+  if (rows.length && lastRow.date === date && lastRow.value === newBalance) {
+    console.log(`row ${date}: ${newBalance} already recorded. abort.`);
+  } else {
+    let newRow = rows[lastRecordIndex - 1];
+    const time = new Date().toISOString().substr(11, 8);
+    const oldBalance = getDouble(lastRow.value);
+    let totalPaid = getDouble(lastRow['total payments']);
+    newBalance = getDouble(newBalance);
 
-		// Record if new premium payment was made:
-		if (getDateFromStr(lastRow.date) < getDateFromStr(premiumDate)) {
-			totalPaid += premiumValue;
-			const intermediateBalance = roundTo2DecimalPlaces(oldBalance + premiumValue);
-			const paymentRowData = {
-				time,
-				date: premiumDate,
-				value: intermediateBalance,
-				change: premiumValue,
-				payment: true,
-				"total payments": roundTo2DecimalPlaces(totalPaid),
-				"total gain": getDouble(lastRow["total gain"]),
-				"rate of return": parseFloat(lastRow["rate of return"]) / 100
-			};
-			saveSheetRow(newRow, paymentRowData);
+    // Record if new premium payment was made:
+    if (getDateFromStr(lastRow.date) < getDateFromStr(premiumDate)) {
+      totalPaid += premiumValue;
+      const intermediateBalance = roundTo2dp(oldBalance + premiumValue);
+      const paymentRowData = {
+        time,
+        'date': premiumDate,
+        'value': intermediateBalance,
+        'change': premiumValue,
+        'payment': true,
+        'total payments': roundTo2dp(totalPaid),
+        'total gain': getDouble(lastRow['total gain']),
+        'rate of return': parseFloat(lastRow['rate of return']) / 100,
+      };
+      saveSheetRow(newRow, paymentRowData);
 
-			lastRow = newRow;
-			newRow = rows[lastRecordIndex - 2];
-		}
+      lastRow = newRow;
+      newRow = rows[lastRecordIndex - 2];
+    }
 
-		// Record new balance:
-		const change = roundTo2DecimalPlaces(newBalance - oldBalance);
-		const totalGain = newBalance - totalPaid;
-		const rateOfReturn = totalGain / totalPaid;
+    // Record new balance:
+    const change = roundTo2dp(newBalance - oldBalance);
+    const totalGain = newBalance - totalPaid;
+    const rateOfReturn = totalGain / totalPaid;
 
-		const newRowData = {
-			time,
-			date,
-			value: newBalance,
-			change,
-			"total payments": lastRow["total payments"],
-			"total gain": totalGain,
-			"rate of return": rateOfReturn
-		};
+    const newRowData = {
+      time,
+      date,
+      'value': newBalance,
+      change,
+      'total payments': lastRow['total payments'],
+      'total gain': totalGain,
+      'rate of return': rateOfReturn,
+    };
 
-		saveSheetRow(newRow, newRowData);
-	}
+    saveSheetRow(newRow, newRowData);
+  }
 }
 
 async function saveSheetRow(newRow, newRowData) {
-	Object.assign(newRow, newRowData);
-	await newRow.save();
+  Object.assign(newRow, newRowData);
+  await newRow.save();
 }
 
 function getDouble(newBalanceText) {
-	return parseFloat(newBalanceText.substring(1).replace(',', ""));
+  return parseFloat(newBalanceText.substring(1).replace(',', ''));
 }
 
 function getDateFromStr(dateStr) {
-	const parts = dateStr.split("/");
-	return new Date(parseInt(parts[2], 10),
-		parseInt(parts[1], 10) - 1,
-		parseInt(parts[0], 10));
+  const parts = dateStr.split('/');
+  return new Date(parseInt(parts[2], 10),
+      parseInt(parts[1], 10) - 1,
+      parseInt(parts[0], 10));
 }
 
-function getFormattedDate(dateText) {
-	return new Date(dateText).toLocaleDateString('en-GB');
+function getDateStr(dateText) {
+  return new Date(dateText).toLocaleDateString('en-GB');
 }
 
-function roundTo2DecimalPlaces(number) {
-	return Math.round((number + Number.EPSILON) * 100) / 100;
+function roundTo2dp(number) {
+  return Math.round((number + Number.EPSILON) * 100) / 100;
 }
